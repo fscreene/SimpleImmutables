d792cf5de79877f48b73b20d3b9fecba739c0cb6
Francis <francis.screene@protonmail.com>
2020-08-02 12:41:22 +0100

    New Test

--- processor/src/test/java/ImmutableTypeVisitorTest.java
+++ processor/src/test/java/ImmutableTypeVisitorTest.java
@@ -2,6 +2,7 @@ import com.fscreene.simpleimmutables.processor.ImmutableTypeVisitor;
 import com.squareup.javapoet.TypeName;
 import org.junit.jupiter.api.Test;
 
+import javax.lang.model.type.PrimitiveType;
 import javax.lang.model.type.TypeMirror;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -11,6 +12,12 @@ public class ImmutableTypeVisitorTest {
     @Test
     public void testVisitThrows() {
         assertThatThrownBy(() -> new ImmutableTypeVisitor().visit(mock(TypeMirror.class), mock(TypeName.class)))
+                .isInstanceOf(UnsupportedOperationException.class);
+    }
+
+    @Test
+    public void testVisitPrimitiveThrows() {
+        assertThatThrownBy(() -> new ImmutableTypeVisitor().visitPrimitive(mock(PrimitiveType.class), mock(TypeName.class)))
                 .isInstanceOf(UnsupportedOperationException.class);
     }
 }
